0dc97260c46b5b6e11a8db813f8e04c8c0a43c07
letheduy1209-coder <letheduy1209@gmail.com>
2026-05-24 17:41:25 +0700

    All staffContronller for CarRentalSystem

---
+++ CarRentalSystem/CarRentalSystem/Controllers/StaffController.cs
@@ -0,0 +1,39 @@
+﻿using CarRentalSystem.Data;
+using CarRentalSystem.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace CarRentalSystem.Controllers
+{
+    public class StaffController : Controller
+    {
+        private readonly CarRentalContext _context;
+
+        public StaffController(CarRentalContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var role = HttpContext.Session.GetString("RoleName");
+            if (role != "Admin") return RedirectToAction("Login", "Account");
+
+            // Lấy danh sách nhân viên kèm thông tin cá nhân và tài khoản
+            var staffs = _context.Staff
+                .Include(s => s.UserProfile)
+                    .ThenInclude(u => u.Account)
+                .OrderByDescending(s => s.StaffId)
+                .ToList();
+
+            // Lấy thêm tổng doanh thu tháng này để hiển thị trên thẻ báo cáo
+            ViewBag.TotalRevenue = _context.Invoices
+                .Where(i => i.Status == "Da thanh toan" || i.Status == "Đã thanh toán")
+                .Sum(i => (decimal?)i.GrandTotal) ?? 0;
+
+            return View("AdminIndex", staffs);
+        }
+    }
+}
