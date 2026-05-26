b1f1c58d30fef1e4db151a236a45c5d01485e3a1
letheduy1209-coder <letheduy1209@gmail.com>
2026-05-26 17:40:51 +0700

    add file datetimeExtensions for Extensions

---
+++ CarRentalSystem/CarRentalSystem/Views/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarRentalSystem.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToVnFormat(this DateTime date)
+        {
+            return date.ToString("dd/MM/yyyy HH:mm");
+        }
+    }
+}
