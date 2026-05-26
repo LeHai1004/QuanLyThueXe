6416e59a379a124aa1d74cec0a3c886dcbfffb79
catraconnguyen76-cloud <Catraconnguyen76@gmail.com>
2026-05-26 21:40:19 +0700

    Add Constants, Helpers, and Validation classes

---
+++ CarRentalSystem/CarRentalSystem/Validation/FutureDateAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarRentalSystem.Validation
+{
+    public class FutureDateAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is DateTime date)
+            {
+                // Chỉ lấy phần ngày (bỏ qua giờ/phút) để so sánh với ngày hôm nay
+                if (date.Date < DateTime.Now.Date)
+                {
+                    return new ValidationResult(ErrorMessage ?? "Ngày không được ở trong quá khứ.");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
